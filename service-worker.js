f4d00d71e567fd1c66799993b64fce8cb5362cdc
eduzatoni <eduzatoni@outlook.com>
2021-04-08 15:17:03 +0200

    add sdk version at service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-self.importScripts("https://assets.emarsys.net/web-emarsys-sdk/<VERSION>/web-emarsys-service-worker.js")
+self.importScripts("https://assets.emarsys.net/web-emarsys-sdk/2.2.0/web-emarsys-service-worker.js")
