8e3976a556e869c8082b3ce2560eb49835f9232c
eduzatoni <eduzatoni@outlook.com>
2021-04-16 14:25:06 +0200

    add dummy func to service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
 self.importScripts("https://assets.emarsys.net/web-emarsys-sdk/2.2.0/web-emarsys-service-worker.js")
+
+self.addEventListener('install', function(event) {
+    console.log("test")
+});
