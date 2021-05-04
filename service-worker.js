84452ce6ed0f88b5f4c1cd60ed7bf9397e620068
Eduardo Zatoni <45048607+eduzatoni@users.noreply.github.com>
2021-05-04 09:54:55 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
 self.importScripts("https://assets.emarsys.net/web-emarsys-sdk/2.2.0/web-emarsys-service-worker.js")
-
-self.addEventListener('install', function(event) {
-    console.log("test")
-});
