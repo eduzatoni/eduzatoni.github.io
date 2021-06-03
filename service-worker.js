32652e9d8ae277b311e658171420ad9224462f6e
Eduardo Zatoni <45048607+eduzatoni@users.noreply.github.com>
2021-06-03 10:04:09 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,2 @@
 self.importScripts("https://assets.emarsys.net/web-emarsys-sdk/2.2.0/web-emarsys-service-worker.js");
-"function"===typeof importScripts&&importScripts("https://api.useinsider.com/sw.js");
+self.importScripts("https://api.useinsider.com/sw.js");
